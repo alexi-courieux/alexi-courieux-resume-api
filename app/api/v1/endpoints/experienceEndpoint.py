2cb1470bf3aef5fa21a2adead219cc4c7e3dfc1b
alexi-courieux <contact@alexicourieux.com>
2025-02-03 15:16:27 -0500

    feat: add endpoint to list experiences and update read_experience to handle language query

--- app/api/v1/endpoints/experienceEndpoint.py
+++ app/api/v1/endpoints/experienceEndpoint.py
@@ -1,13 +1,20 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
 from app.dependencies import get_db
 from app.schemas.experienceSchema import ExperienceSchema
-from app.crud.experienceCrud import get_experience
+from app.crud.experienceCrud import get_experience, get_experiences
 
 router = APIRouter()
 @router.get("/{company}", response_model=ExperienceSchema, responses={404: {"description": "Experience not found"}})
-async def read_experience(company: str, language: str = "EN", db: Session = Depends(get_db)):
+async def read_experience(company: str, language: str = Query("EN"), db: Session = Depends(get_db)):
+    language = language.upper()
     experience = get_experience(db, company, language)
     if not experience:
         raise HTTPException(status_code=404, detail="Experience not found")
     return experience
+
+@router.get("/", response_model=list[ExperienceSchema])
+async def list_experiences(language: str = Query("EN"), db: Session = Depends(get_db)):
+    language = language.upper()
+    print(language)
+    return get_experiences(db, language)
--- app/crud/experienceCrud.py
+++ app/crud/experienceCrud.py
@@ -1,7 +1,8 @@
 from sqlalchemy import text
 from sqlalchemy.orm import Session
+from app.schemas.experienceSchema import ExperienceSchema
 
-def get_experience(db: Session, company: str, language: str):
+def get_experience(db: Session, company: str, language: str) -> ExperienceSchema:
     sql = text("""
     SELECT e.COMPANY as company, ei18n.COMPANY_NAME as company_name, ei18n.POSITION as position, e.START_DATE as start_date, e.END_DATE as end_date, ei18n.SHORT_DESCRIPTION as short_description, ei18n.DESCRIPTION as description
     FROM EXPERIENCE e
@@ -10,4 +11,32 @@ def get_experience(db: Session, company: str, language: str):
     LIMIT 1
     """)
     result = db.execute(sql, {"company": company, "language": language}).fetchone()
-    return result
+    if result:
+        return ExperienceSchema(
+            company=result.company,
+            companyName=result.company_name,
+            position=result.position,
+            startDate=result.start_date,
+            endDate=result.end_date,
+            shortDescription=result.short_description,
+            description=result.description
+        )
+    return None
+
+def get_experiences(db: Session, language: str) -> list[ExperienceSchema]:
+    sql = text("""
+    SELECT e.COMPANY as company, ei18n.COMPANY_NAME as company_name, ei18n.POSITION as position, e.START_DATE as start_date, e.END_DATE as end_date, ei18n.SHORT_DESCRIPTION as short_description, ei18n.DESCRIPTION as description
+    FROM EXPERIENCE e
+    JOIN EXPERIENCE_I18N ei18n ON e.COMPANY = ei18n.EXPERIENCE_COMPANY
+    WHERE ei18n.LANGUAGE = :language
+    """)
+    results = db.execute(sql, {"language": language}).fetchall()
+    return [ExperienceSchema(
+        company=result.company,
+        companyName=result.company_name,
+        position=result.position,
+        startDate=result.start_date,
+        endDate=result.end_date,
+        shortDescription=result.short_description,
+        description=result.description
+    ) for result in results]
